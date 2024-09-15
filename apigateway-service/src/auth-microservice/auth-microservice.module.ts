69cad3b07e6412a8a38b1c69d71c87e563cdfa34
Azutech <azunna.onugha@gmail.com>
2024-09-15 13:36:47 +0100

    feat () : Authservice removed

--- apigateway-service/src/auth-microservice/auth-microservice.module.ts
+++ apigateway-service/src/auth-microservice/auth-microservice.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
-import { AuthMicroserviceService } from './auth-microservice.service';
 import { AuthMicroserviceController } from './auth-microservice.controller';
 import { HttpModule } from '@nestjs/axios'; // Import HttpModule
 
@@ -16,26 +14,9 @@ import { HttpModule } from '@nestjs/axios'; // Import HttpModule
       }),
       inject: [ConfigService],
     }),
-    ClientsModule.registerAsync([
-      {
-        name: 'AUTH_SERVICE',
-        inject: [ConfigService],
-        useFactory: async (config: ConfigService) => ({
-          transport: Transport.RMQ,
-          options: {
-            urls: [
-              'amqps://fwruvcta:TzXM69bOmQr02gmCL8p4MUMuPKf5r5xg@cow.rmq2.cloudamqp.com/fwruvcta',
-            ],
-            queue: 'auth_microservice',
-            queueOptions: {
-              durable: true,
-            },
-          },
-        }),
-      },
-    ]),
+  
   ],
   controllers: [AuthMicroserviceController],
-  providers: [AuthMicroserviceService],
+  providers: [],
 })
 export class AuthMicroserviceModule {}
